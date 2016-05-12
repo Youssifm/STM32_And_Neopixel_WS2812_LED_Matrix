e2f97d432357eb7eea6f2c310012e3d18f7400a1
Youssifm <Yoma1993@gmail.com>
2016-05-12 00:22:33 -0500

    Formatting test for whitespace
    Formatting test for whitespace

--- User/vars.h
+++ User/vars.h
@@ -1,17 +1,17 @@
 #include <stm32f10x.h>
 #include <stdbool.h>
 
-#define GPIO_PWM_PIN				GPIO_Pin_2
-#define GPIO_USART_TX_PIN			GPIO_Pin_9
-#define GPIO_USART_RX_PIN			GPIO_Pin_10
-#define GPIO_ADC_PIN				GPIO_Pin_0
+#define GPIO_PWM_PIN                GPIO_Pin_2
+#define GPIO_USART_TX_PIN           GPIO_Pin_9
+#define GPIO_USART_RX_PIN           GPIO_Pin_10
+#define GPIO_ADC_PIN                GPIO_Pin_0
 
-#define PWM_TIM_PERIOD				29								// Number of CPU cycles that will constitute 1 period
-#define PWM_HIGH_WIDTH				17								// Duty cycle of pwm signal for a logical 1 to be read by the ws2812 chip. Duty cycle = PWM_HIGH_WIDTH/TIM_PERIOD*100
-#define PWM_LOW_WIDTH				9								// Duty cycle of pwm signal for a logical 0 to be read by the ws2812 chip. Duty cycle = PWM_LOW_WIDTH/TIM_PERIOD*100
+#define PWM_TIM_PERIOD              29                              // Number of CPU cycles that will constitute 1 period
+#define PWM_HIGH_WIDTH              17                              // Duty cycle of pwm signal for a logical 1 to be read by the ws2812 chip. Duty cycle = PWM_HIGH_WIDTH/TIM_PERIOD*100
+#define PWM_LOW_WIDTH               9                               // Duty cycle of pwm signal for a logical 0 to be read by the ws2812 chip. Duty cycle = PWM_LOW_WIDTH/TIM_PERIOD*100
 
-#define COLUMBS						8
-#define	ROWS						1
+#define COLUMBS                     8
+#define ROWS                        1
 
 /* Buffer that holds one complete DMA transmission.
  *
@@ -22,15 +22,15 @@
  * (2^16 bytes per DMA stream - 42 bytes)/24 bytes per LED = 2728 LEDs.
  */
 
-#define LED_COUNT			COLUMBS*ROWS
-#define LED_BUFFER_SIZE		24*LED_COUNT+42			// Buffer size needs to be the number of LEDs times 24 bits plus 42 trailing bit to signify the end of the data being transmitted.
+#define LED_COUNT           COLUMBS*ROWS
+#define LED_BUFFER_SIZE     24*LED_COUNT+42         // Buffer size needs to be the number of LEDs times 24 bits plus 42 trailing bit to signify the end of the data being transmitted.
 
-extern uint8_t rgb[LED_COUNT][3];					//Array that will store color data
-extern uint8_t led_Colors[LED_COUNT];				//Array of integres that will function as indexes for the rgb array
-extern uint16_t ledBuff[LED_BUFFER_SIZE];			//Array of data to be sent to leds.
+extern uint8_t rgb[LED_COUNT][3];                   //Array that will store color data
+extern uint8_t led_Colors[LED_COUNT];               //Array of integres that will function as indexes for the rgb array
+extern uint16_t ledBuff[LED_BUFFER_SIZE];           //Array of data to be sent to leds.
 extern uint32_t usart_Baud_Rate;
 extern char usartBTBuffer[25];
 extern bool config_BT;
 
-extern char usartBuff[LED_BUFFER_SIZE+100];			// Debugging buffer
+extern char usartBuff[LED_BUFFER_SIZE+100];         // Debugging buffer
 
